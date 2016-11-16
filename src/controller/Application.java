d7df4dc7c04edbe86c0628b1827b72b9efa73fb2
Filip Mazurek <fjm7@duke.edu>
2016-11-16 15:07:28 -0500

    Initial commit with base example code

---
+++ src/controller/Application.java
@@ -0,0 +1,17 @@
+package controller;
+
+        import java.sql.SQLException;
+        import java.util.Map;
+        import java.util.ArrayList;
+
+        import play.*;
+        import play.mvc.*;
+        import play.data.*;
+
+        import views.html.*;
+
+        import models.BeerDB;
+
+public class Application extends Controller {
+
+}
